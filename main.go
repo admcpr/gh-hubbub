1275a4b608b7f20263abbde7cf58a747690927ca
Adam <mail@admcpr.com>
2024-03-29 09:59:20 +0100

    💫

--- main.go
+++ main.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 	"os"
 
-	"gh-hubbub/consts"
 	"gh-hubbub/models"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	models.MainModel = []tea.Model{models.NewUserModel(), &models.OrgModel{}}
+	// models.MainModel = []tea.Model{models.NewUserModel(), &models.OrgModel{}}
+	mainModelV2 := models.NewMainModelV2()
 
-	p := tea.NewProgram(models.MainModel[consts.UserModelName])
+	// p := tea.NewProgram(models.MainModel[consts.UserModelName])
+	p := tea.NewProgram(mainModelV2)
 	// p := tea.NewProgram(models.NewFilterBooleanModel("Is this a boolean?", true))
 	// p := tea.NewProgram(models.NewFilterNumberModel("What number would you like to choose?", 1, 1))
 	// p := tea.NewProgram(models.NewFilterDateModel("What date would you like to choose?", time.Now(), time.Now()))
@@ -23,6 +24,3 @@ func main() {
 		os.Exit(1)
 	}
 }
-
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
--- models/mainModel.go
+++ models/mainModel.go
@@ -1,5 +1,50 @@
 package models
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 var MainModel []tea.Model
+
+type MainModelV2 struct {
+	spinner   spinner.Model
+	UserModel UserModel
+	OrgModel  OrgModel
+	RepoModel RepoModel
+}
+
+func NewMainModelV2() MainModelV2 {
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	return MainModelV2{
+		spinner: s,
+	}
+}
+
+func (m MainModelV2) Init() tea.Cmd {
+	return m.spinner.Tick
+}
+
+func (m MainModelV2) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl+c":
+			return m, tea.Quit
+		default:
+			return m, nil
+		}
+	default:
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+}
+
+func (m MainModelV2) View() string {
+	return fmt.Sprintf("%s Authenticating ... \n\n", m.spinner.View())
+}
