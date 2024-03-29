a342a374f9f06c1c2eee635b67dd2b7904b8e6ad
Adam <mail@admcpr.com>
2024-03-29 13:26:24 +0100

    get

--- models/mainModel.go
+++ models/mainModel.go
@@ -2,9 +2,11 @@ package models
 
 import (
 	"fmt"
+	"gh-hubbub/structs"
 
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/cli/go-gh/v2/pkg/api"
 )
 
 var MainModel []tea.Model
@@ -14,6 +16,8 @@ type MainModelV2 struct {
 	UserModel UserModel
 	OrgModel  OrgModel
 	RepoModel RepoModel
+
+	User structs.User
 }
 
 func NewMainModelV2() MainModelV2 {
@@ -25,7 +29,9 @@ func NewMainModelV2() MainModelV2 {
 }
 
 func (m MainModelV2) Init() tea.Cmd {
-	return m.spinner.Tick
+	cmds := []tea.Cmd{m.spinner.Tick, getUser}
+
+	return tea.Batch(cmds...)
 }
 
 func (m MainModelV2) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -47,4 +53,20 @@ func (m MainModelV2) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m MainModelV2) View() string {
 	return fmt.Sprintf("%s Authenticating ... \n\n", m.spinner.View())
+}
+
+func getUser() tea.Msg {
+	client, err := api.DefaultRESTClient()
+	if err != nil {
+		return AuthenticationErrorMsg{Err: err}
+	}
+	response := structs.User{}
+
+	err = client.Get("user", &response)
+	if err != nil {
+		fmt.Println(err)
+		return AuthenticationErrorMsg{Err: err}
+	}
+
+	return AuthenticationMsg{User: response}
 }
