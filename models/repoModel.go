e6dbd4a246eca1ab1c06fe9709799c20244e35e6
adam <hello@admcpr.com>
2024-10-24 17:51:52 +0200

    Use a pager and add titles to the repo model

--- models/repoModel.go
+++ models/repoModel.go
@@ -4,6 +4,7 @@ import (
 	"gh-hubbub/structs"
 	"gh-hubbub/style"
 
+	"github.com/charmbracelet/bubbles/paginator"
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -12,17 +13,25 @@ import (
 type RepoModel struct {
 	repository    structs.RepositorySettings
 	settingsTable table.Model
-
-	activeTab int
-	width     int
-	height    int
+	paginator     paginator.Model
+	activeTab     int
+	width         int
+	height        int
 }
 
 func NewRepoModel(width, height int) RepoModel {
+	p := paginator.New()
+	p.Type = paginator.Dots
+	p.PerPage = 1
+	p.ActiveDot = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "235", Dark: "252"}).Render("•")
+	p.InactiveDot = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "250", Dark: "238"}).Render("•")
+	p.SetTotalPages(4)
+
 	return RepoModel{
 		repository: structs.RepositorySettings{},
 		width:      width,
 		height:     height,
+		paginator:  p,
 	}
 }
 
@@ -44,6 +53,7 @@ func (m *RepoModel) SelectRepo(repository structs.RepositorySettings) {
 }
 
 func (m *RepoModel) SelectTab(index int) {
+	m.paginator.Page = index
 	m.activeTab = index
 	m.settingsTable = NewSettingsTable(m.repository.SettingsTabs[m.activeTab].Settings, m.width)
 }
@@ -55,9 +65,17 @@ func (m RepoModel) Update(msg tea.Msg) (RepoModel, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyTab:
-			m.SelectTab(min(m.activeTab+1, len(m.repository.SettingsTabs)-1))
+			if m.activeTab < len(m.repository.SettingsTabs)-1 {
+				m.SelectTab(m.activeTab + 1)
+			} else {
+				m.SelectTab(0)
+			}
 		case tea.KeyShiftTab:
-			m.SelectTab(max(m.activeTab-1, 0))
+			if m.activeTab > 0 {
+				m.SelectTab(m.activeTab - 1)
+			} else {
+				m.SelectTab(len(m.repository.SettingsTabs) - 1)
+			}
 		}
 	}
 	return m, cmd
@@ -71,12 +89,14 @@ func (m RepoModel) View() string {
 
 	// frameWidth, frameHeight := style.Settings.GetFrameSize()
 
+	header := m.paginator.View() + " " + m.repository.SettingsTabs[m.activeTab].Name
+
 	settings := style.App.
 		Width(m.width).
 		// Height(5).
 		Render(m.settingsTable.View())
 
-	return lipgloss.JoinVertical(lipgloss.Left, settings)
+	return lipgloss.JoinVertical(lipgloss.Left, header, settings)
 }
 
 func NewSettingsTable(activeSettings []structs.Setting, width int) table.Model {
