cb9325d483898a644177a0827283eeec45452d57
adam <hello@admcpr.com>
2025-01-21 19:56:44 +0100

    Test filters

---
+++ filters/boolFilter_test.go
@@ -0,0 +1,67 @@
+package filters
+
+import (
+	"fmt"
+	"gh-hubbub/structs"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type BoolFilterSuite struct {
+	suite.Suite
+	filter BoolFilter
+}
+
+func (s *BoolFilterSuite) SetupTest() {
+	s.filter = NewBoolFilter("test", true)
+}
+
+func (s *BoolFilterSuite) TestNewBoolFilter() {
+	filter := NewBoolFilter("example", true)
+	s.Equal("example", filter.Name)
+	s.True(filter.Value)
+}
+
+func (s *BoolFilterSuite) TestGetName() {
+	s.Equal("test", s.filter.GetName())
+}
+
+func (s *BoolFilterSuite) TestMatches() {
+	tests := []struct {
+		name     string
+		property structs.RepoProperty
+		want     bool
+	}{
+		{
+			name:     "matching bool value",
+			property: structs.RepoProperty{Type: "bool", Value: true},
+			want:     true,
+		},
+		{
+			name:     "non-matching bool value",
+			property: structs.RepoProperty{Type: "bool", Value: false},
+			want:     false,
+		},
+		{
+			name:     "non-bool property type",
+			property: structs.RepoProperty{Type: "string", Value: "true"},
+			want:     false,
+		},
+	}
+
+	for _, tt := range tests {
+		s.Run(tt.name, func() {
+			s.Equal(tt.want, s.filter.Matches(tt.property))
+		})
+	}
+}
+
+func (s *BoolFilterSuite) TestString() {
+	expected := fmt.Sprintf("%s: %v", s.filter.Name, s.filter.Value)
+	s.Equal(expected, s.filter.String())
+}
+
+func TestBoolFilterSuite(t *testing.T) {
+	suite.Run(t, new(BoolFilterSuite))
+}
---
+++ filters/dateFilter_test.go
@@ -0,0 +1,110 @@
+package filters
+
+import (
+	"gh-hubbub/structs"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type DateFilterSuite struct {
+	suite.Suite
+	filter   DateFilter
+	baseTime time.Time
+	from     time.Time
+	to       time.Time
+}
+
+func (s *DateFilterSuite) SetupTest() {
+	s.baseTime = time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
+	s.from = s.baseTime
+	s.to = s.baseTime.AddDate(0, 1, 0) // one month later
+	s.filter = NewDateFilter("test", s.from, s.to)
+}
+
+func (s *DateFilterSuite) TestNewDateFilter() {
+	filter := NewDateFilter("example", s.from, s.to)
+	s.Equal("example", filter.Name)
+	s.Equal(s.from, filter.From)
+	s.Equal(s.to, filter.To)
+}
+
+func (s *DateFilterSuite) TestGetName() {
+	s.Equal("test", s.filter.GetName())
+}
+
+func (s *DateFilterSuite) TestMatches() {
+	tests := []struct {
+		name     string
+		property structs.RepoProperty
+		want     bool
+	}{
+		{
+			name: "date within range",
+			property: structs.RepoProperty{
+				Type:  "time.Time",
+				Value: s.baseTime.AddDate(0, 0, 15),
+			},
+			want: true,
+		},
+		{
+			name: "date before range",
+			property: structs.RepoProperty{
+				Type:  "time.Time",
+				Value: s.baseTime.AddDate(0, 0, -1),
+			},
+			want: false,
+		},
+		{
+			name: "date after range",
+			property: structs.RepoProperty{
+				Type:  "time.Time",
+				Value: s.baseTime.AddDate(0, 2, 0),
+			},
+			want: false,
+		},
+		{
+			name: "date on start boundary",
+			property: structs.RepoProperty{
+				Type:  "time.Time",
+				Value: s.from,
+			},
+			want: true,
+		},
+		{
+			name: "date on end boundary",
+			property: structs.RepoProperty{
+				Type:  "time.Time",
+				Value: s.to,
+			},
+			want: true,
+		},
+		{
+			name: "invalid property type",
+			property: structs.RepoProperty{
+				Type:  "string",
+				Value: "2024-01-01",
+			},
+			want: false,
+		},
+		{
+			name: "zero time",
+			property: structs.RepoProperty{
+				Type:  "time.Time",
+				Value: time.Time{},
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		s.Run(tt.name, func() {
+			s.Equal(tt.want, s.filter.Matches(tt.property))
+		})
+	}
+}
+
+func TestDateFilterSuite(t *testing.T) {
+	suite.Run(t, new(DateFilterSuite))
+}
